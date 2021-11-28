1c754c10fcf2d7f5217fc3d38aa5131fe06f3881
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2021-11-28 12:58:42 +0300

    passHref added to Link component

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,24 +11,24 @@ const Header = () => {
             <li className="burgerButton">
               <FaBars className="icon" size="1.5rem" />
             </li>
-            <Link href="/">
+            <Link href="/" passHref>
               <li className="sitename">Uzm. Psk. Özge Demirsu</li>
             </Link>
           </ul>
           <div className="navBarRight">
             <ul className={styles.navLinks}>
               <li>
-                <Link href="/">
+                <Link href="/" passHref>
                   <a id="homeLink">Anasayfa</a>
                 </Link>
               </li>
               <li>
-                <Link href="/about">
+                <Link href="/about" passHref>
                   <a id="aboutLink">Hakkımda</a>
                 </Link>
               </li>
               <li>
-                <Link href="/contact">
+                <Link href="/contact" passHref>
                   <a id="contactLink">İletişim</a>
                 </Link>
               </li>
