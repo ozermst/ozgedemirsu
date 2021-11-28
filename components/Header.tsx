54ed2deaf31b9a56c070bbe28127964c2c73be96
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2021-11-28 16:45:53 +0300

    mobile menu added. Spacer div commented out

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "../styles/Header.module.css";
 
+import toggleMenu from "../js/toggleMenu";
+
 const Header = () => {
   const router = useRouter();
   return (
@@ -10,14 +12,16 @@ const Header = () => {
       <header className={styles.header}>
         <div className={styles.navBar}>
           <ul className={styles.navBarLeft}>
-            <li className="burgerButton">
+            <li className={styles.burgerButton} onClick={toggleMenu}>
               <FaBars className="icon" size="1.5rem" />
             </li>
-            <Link href="/" passHref>
-              <li className="sitename">Uzm. Psk. Özge Demirsu</li>
-            </Link>
+            <li className="sitename">
+              <Link href="/" passHref>
+                <a>Uzm. Psk. Özge Demirsu</a>
+              </Link>
+            </li>
           </ul>
-          <div className="navBarRight">
+          <div className={styles.navBarRight}>
             <ul className={styles.navLinks}>
               <li className={router.pathname == "/" ? styles.active : ""}>
                 <Link href="/" passHref>
@@ -37,9 +41,43 @@ const Header = () => {
                 </Link>
               </li>
             </ul>
+            <div className="signUp"></div>
           </div>
         </div>
+        <div className="mobileMenu" id="mobileMenu" onClick={toggleMenu}>
+          <ul className={styles.mobileMenuLinks}>
+            <li>
+              <Link href="/" passHref>
+                <a id="homeLink">Anasayfa</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" passHref>
+                <a id="aboutLink">Hakkımızda</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" passHref>
+                <a id="contactLink">İletişim</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </header>
+      <style jsx>{`
+        .mobileMenu {
+          color: white;
+          display: none;
+        }
+        .show {
+          display: block;
+        }
+        @media screen and (min-width: 640px) {
+          .mobileMenu {
+            display: none;
+          }
+        }
+      `}</style>
     </>
   );
 };
---
+++ js/toggleMenu.ts
@@ -0,0 +1,17 @@
+const toggleMenu = () => {
+    let mobileMenu = document.getElementById("mobileMenu");
+
+    if (mobileMenu!=null){
+        if (mobileMenu.className.indexOf("show") == -1) {
+          mobileMenu.className += " show";
+          } else {
+            mobileMenu.className = mobileMenu.className.replace(
+              "show",
+              " "
+            );
+          }
+    }
+    
+  }
+
+  export default toggleMenu
