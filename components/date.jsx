5361661b67b56fe3affbbbe01042b7b7818a5ca7
Mustafa Ozer <mustafa.ozer@cento.com.tr>
2021-12-16 18:45:45 +0300

    tr date conversion

--- components/date.jsx
+++ components/date.jsx
@@ -1,7 +1,12 @@
 import { parseISO, format } from "date-fns";
+import { tr } from "date-fns/locale";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
 
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  return (
+    <time dateTime={dateString}>
+      {format(date, "LLLL	d, yyyy", { locale: tr })}
+    </time>
+  );
 }
